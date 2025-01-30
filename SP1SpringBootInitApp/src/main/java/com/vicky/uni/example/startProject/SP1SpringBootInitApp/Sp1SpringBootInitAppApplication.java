09cabe3b16858bfeef1e1a8409f7692d38af0e5d
vikkymandloi <imvrajendra@gmail.com>
2025-01-31 00:45:23 +0530

    first commit

---
+++ SP1SpringBootInitApp/src/main/java/com/vicky/uni/example/startProject/SP1SpringBootInitApp/Sp1SpringBootInitAppApplication.java
@@ -0,0 +1,13 @@
+package com.vicky.uni.example.startProject.SP1SpringBootInitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp1SpringBootInitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp1SpringBootInitAppApplication.class, args);
+	}
+
+}
